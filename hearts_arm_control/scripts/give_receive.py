1e789f866be708920be9cef94fb9e66204b6ab01
Daniel Bellamy <dd17883@my.bristol.ac.uk>
2018-09-19 16:07:04 +0100

    Added custom motions ros node, configuration & launch file

--- hearts_arm_control/scripts/give_receive.py
+++ hearts_arm_control/scripts/give_receive.py
@@ -6,12 +6,21 @@ from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from sensor_msgs.msg import JointState
 from std_msgs.msg import String
 
+def do_it(x):
+#create the play motion goal and tell it which motion to perform
+    pmg = PlayMotionGoal()
+    pmg.motion_name = x
+    pmg.skip_planning = False
+#tell the client to send the goal
+    pmc.send_goal(pmg)    
 
 
-rospy.init_node("give_receive")
-pms = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
-pms.wait_for_server
+pmc = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
+pmc.wait_for_server
+
+rospy.subscriber("motion_name", String, do_it)
 
 
 
-#if __name__ == '__main__':    
+if __name__ == '__main__':    
+    rospy.init_node("give_receive")
