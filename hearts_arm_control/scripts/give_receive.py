d6c65332062c9e2e9eeb2222eee47a2edbc6e906
Numjerlunker <rwakelingg@gmail.com>
2018-09-21 19:50:09 +0100

    fixed data type in callback function

--- hearts_arm_control/scripts/give_receive.py
+++ hearts_arm_control/scripts/give_receive.py
@@ -6,18 +6,19 @@ from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from sensor_msgs.msg import JointState
 from std_msgs.msg import String
 
-def do_it(x):
+def do_it(name):
 #create the play motion goal and tell it which motion to perform
     pmg = PlayMotionGoal()
-    pmg.motion_name = x
+    pmg.motion_name = name.data
     pmg.skip_planning = False
 #tell the client to send the goal
-    pmc.send_goal(pmg)    
+    pmc.send_goal_and_wait(pmg)    
 
 rospy.init_node("give_receive")
 pmc = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
-pmc.wait_for_server
+pmc.wait_for_server()
 
+#Subscriber to get motion name from '/motion_name' topic
 rospy.Subscriber("motion_name", String, do_it)
 
 rospy.spin()
